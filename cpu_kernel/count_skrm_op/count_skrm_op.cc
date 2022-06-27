975321e615f9a92f517588ffdd85f1fcb9577a58
DandinPower <t108590045@ntut.org.tw>
2022-06-27 10:32:17 +0800

    fix bug

---
+++ cpu_kernel/count_skrm_op/count_skrm_op.cc
@@ -0,0 +1,43 @@
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/framework/op_kernel.h"
+
+using namespace tensorflow;
+
+REGISTER_OP("CountSkrm")
+    .Input("to_zero: float")
+    .Output("zeroed: float")
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+      c->set_output(0, c->input(0));
+      return Status::OK();
+    });
+
+class CountSkrmOp : public OpKernel {
+ public:
+  explicit CountSkrmOp(OpKernelConstruction* context) : OpKernel(context) {}
+
+  void Compute(OpKernelContext* context) override {
+    // Grab the input tensor
+    const Tensor& input_tensor = context->input(0);
+    //const Tensor& input_tensor2 = context->input(1);
+    auto input = input_tensor.flat<float>();
+    //auto input2 = input_tensor2.flat<float>();
+
+    // Create an output tensor
+    Tensor* output_tensor = NULL;
+    OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor.shape(),
+                                                     &output_tensor));
+    auto output_flat = output_tensor->flat<float>();
+
+    // Set all but the first element of the output tensor to 0.
+    const int N = input.size();
+    for (int i = 1; i < N; i++) {
+      output_flat(i) = 0.0;
+    }
+
+    // Preserve the first input value if possible.
+    if (N > 0) output_flat(0) = input(0);
+  }
+};
+
+REGISTER_KERNEL_BUILDER(Name("CountSkrm").Device(DEVICE_CPU), CountSkrmOp);
