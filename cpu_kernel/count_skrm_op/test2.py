45ae11b4805a59a477265349945bc5f4e2786120
DandinPower <tomhot246@gmail.com>
2022-07-05 08:59:37 +0800

    adjust testcase

--- cpu_kernel/count_skrm_op/test2.py
+++ cpu_kernel/count_skrm_op/test2.py
@@ -1,12 +1,20 @@
 import tensorflow as tf
 kernal_module = tf.load_op_library('./count_skrm.so')
-tensorA = tf.constant([1.123,3.123321])
-tensorB = tf.constant([-0.323231])
 tensorShape = tf.zeros([8],tf.int64)
+
+#test case 1 (前面資料量較大)
+tensorA = tf.constant([1.2, 3.1])
+tensorB = tf.constant([-0.3])
 print(kernal_module.count_skrm(tensorA,tensorB,tensorShape))
-tensorA = tf.constant([1.123,3.123321])
-tensorB = tf.constant([-0.323231,4.132231])
+
+#test case 2 (資料量一樣大)
+tensorA = tf.constant([1.2, 3.1])
+tensorB = tf.constant([-0.3, 4.1])
 print(kernal_module.count_skrm(tensorA,tensorB,tensorShape))
-tensorA = tf.constant([1.123])
-tensorB = tf.constant([-0.323231,3.123321])
+
+#test case 3 (後面資料量較大)
+tensorA = tf.constant([1.1])
+tensorB = tf.constant([-0.3, 3.1])
 print(kernal_module.count_skrm(tensorA,tensorB,tensorShape))
+
+
